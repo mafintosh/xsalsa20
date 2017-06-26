9429b5cbe78af2bb078fab68b0e79c91726e8662
Mathias Buus <mathiasbuus@gmail.com>
2017-06-26 18:08:26 +0200

    wasm works

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # xsalsa20
 
-WIP - nothing to see here
+XSalsa20 implemented in Javascript and WebAssembly.
 
 ```
 npm install xsalsa20
@@ -9,8 +9,34 @@ npm install xsalsa20
 ## Usage
 
 ``` js
+var crypto = require('crypto')
 var xsalsa20 = require('xsalsa20')
+var key = crypto.randomBytes(32)
+var nonce = crypto.randomBytes(24)
+
+var xor = xsalsa20(nonce, key)
+
+console.log(xor.update(new Buffer('hello')))
+console.log(xor.update(new Buffer('world')))
+
+xor.finalize()
 ```
+
+## API
+
+#### `var xor = xsalsa20(nonce, key)`
+
+Create a new xor instance.
+
+Nonce should be a 24 byte buffer/uint8array and key should be 32 bytes.
+
+#### `var output = xor.update(input, [output])`
+
+Update the xor instance with a new input buffer. Optionally you can pass in an output buffer.
+
+#### `xor.finalize()`
+
+Call this method last. Clears internal state.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,28 +1,51 @@
-var fs = require('fs')
-var toUint8Array = require('base64-to-uint8array')
+var xsalsa20 = require('./xsalsa20')()
 
-var WASM = toUint8Array(fs.readFileSync(__dirname + '/xsalsa20.wasm', 'base64'))
-var mod
-var mem
-var rdy
+var head = 144
+var free = []
 
-exports.WASM_SUPPORTED = typeof WebAssembly !== 'undefined'
+module.exports = XSalsa20
 
-function ready (cb) {
-  if (!cb) cb = noop
-  if (!exports.WASM_SUPPORTED) return cb()
-  if (!rdy) rdy = WebAssembly.instantiate(WASM).then(setup)
-  return rdy.then(cb).catch(cb)
+XSalsa20.NONCEBYTES = 24
+XSalsa20.KEYBYTES = 32
+
+function XSalsa20 (nonce, key) {
+  if (!(this instanceof XSalsa20)) return new XSalsa20(nonce, key)
+  if (!nonce || nonce.length < 24) throw new Error('nonce must be at least 24 bytes')
+  if (!key || key.length < 32) throw new Error('key must be at least 32 bytes')
+
+  if (!free.length) {
+    free.push(head)
+    head += 56
+  }
+
+  this.pointer = free.pop()
+  this.nonce = this.pointer + 8
+  this.key = this.nonce + 16
+  this.overflow = 0
+
+  xsalsa20.memory.fill(0, this.pointer, this.pointer + 8)
+  xsalsa20.memory.set(nonce, this.nonce)
+  xsalsa20.memory.set(key, this.key)
 }
 
-function noop () {}
+XSalsa20.prototype.update = function (input, output) {
+  if (!input) throw new Error('input must be Uint8Array or Buffer')
+  if (!output) output = new Uint8Array(input.length)
 
-function setup (w) {
-  mod = w.instance.exports
-  mem = new Uint8Array(w.instance.exports.memory.buffer)
+  var len = this.overflow + input.length
+  var start = head + this.overflow
+
+  xsalsa20.memory.set(input, start)
+  xsalsa20.exports.xsalsa20_xor(this.pointer, head, head, len, this.nonce, this.key)
+  output.set(xsalsa20.memory.subarray(start, head + len))
+
+  this.overflow = len & 63
+
+  return output
 }
 
-ready(function () {
-  mod.core_hsalsa20(0, 32, 48)
-  console.log(new Buffer(mem.slice(0, 32)))
-})
+XSalsa20.prototype.finalize =
+XSalsa20.prototype.final = function () {
+  xsalsa20.memory.fill(0, this.pointer, this.key + 32)
+  free.push(this.pointer)
+}
